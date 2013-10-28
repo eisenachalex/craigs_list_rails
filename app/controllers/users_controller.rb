aa0a23fafb6fde2895a054b95c722f44b9571d49
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 09:56:10 -0500

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    def new
+    end
+
+    def index
+    end
+
+    # def create
+    #  @user = User.new(post_params)
+    #  @user.save 
+    #  redirect_to @user 
+    # end
+
+    # private 
+    #   def post_params
+    #     params.require(:user).permit(:name, :email, :password)
+    #   end
+end
