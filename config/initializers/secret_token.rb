366fde66027bbc5525894358763d28a7b40b1f3a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 09:17:49 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraigsListRails::Application.config.secret_token = 'c81953716c7f96994f3e1ce85c5c90ceb862f19a0cf06d73f94a7a97b82f0bfb6860d577b232158a05505d6cc0b2e19be0639cfc1f7c567a73af2bd3347f2ca7'
